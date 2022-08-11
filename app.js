6e94a292d565243978a1ed308181366f7f6594e4
BeengBeeng <110025541+BeengBeeng@users.noreply.github.com>
2022-08-11 15:31:01 +0700

    baitap2

---
+++ app.js
@@ -0,0 +1,94 @@
+// bài 1
+// function tong(a, b) {
+//     tong = a + b;
+//     return tong;
+// }
+// let a = Number(prompt("nhập số"));
+// let b = Number(prompt("nhập số"));
+// console.log(tong(a, b));
+
+// bai 2
+
+// function tongLe(array) {
+//     let tong = 0;
+//     for (i = 0; i < array.length; i++);
+//     if (array[i] % 2 === 1) {
+//         tong = tong + tong[i];
+//     }
+//     return tong;
+// }
+// let array = [1, 2, 3, 4, 5, 6];
+// let tong = tongLe(array);
+// console.log(tong);
+
+// bai 3
+// function tong(a, b) {
+//     if (a > b) {
+//         alert(`tham so 1 lon hon tham so 2`);
+//     } else {
+//         let tong = a +b;
+//         return tong;
+//     } if (a===b){
+//         alert (`nhap lai`);
+//     }
+//     console.log(tong(a,b));
+// }
+// let a = Number(prompt("nhập số"));
+// let b = Number(prompt("nhập số"));
+
+// // bai 4
+
+// function addNumbers() {
+//       firstNum = 4;
+//       secondNum = 8;
+//       result = firstNum + secondNum;
+//       return result;
+//     }
+//     result = 0;
+//     alert("result truoc khi goi ham: " + result);
+//     result = addNumbers();
+//     alert("result sau khi goi ham: " + result);
+
+// bài 5
+
+// let foot = Number(prompt("nhap foot :"));
+// function footToMeter(foot){
+//     let meter = 0.305 * foot;
+//     return meter;
+// }
+// console.log(footToMeter(foot));
+
+// let meter = Number(prompt("nhap foot :"));
+// function meterToFoot(meter){
+//     let foot = 3.279 * meter;
+//     return foot;
+// }
+// console.log(meterToFoot(meter));
+
+// bai 6
+
+// function isPrime(number) {
+//     if (number < 2) {
+//         return 0; // mac dinh 0 phai la so nguyen to
+//     } else {
+        // if (number === 2) {
+//             return number;
+//         }
+//         let i = 2
+        // while (i < number) {
+//             if (number % i === 0) {
+//                 return 0;
+//             }
+//             i++;
+//         }
+//         return number;
+//     }
+// }
+
+// for(let i =1; i<=100; i++){
+//     let a = isPrime(i);
+//     if(a !== 0){
+//         console.log(a);
+//     }
+// }
+
